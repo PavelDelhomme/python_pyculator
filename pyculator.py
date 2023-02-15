14b507be759e659fcb33dabbc03d96a08616f79a
PavelDelhomme <pavel@delhomme.ovh>
2023-02-15 14:38:33 +0100

    Cette version améliorée inclut les mêmes fonctionnalités que la version précédente, mais avec un aspect visuel amélioré. Les changements incluent :
        L'ajout d'une couleur de fond pour les boutons et pour l'affichage.
        L'ajout d'une bordure autour de l'affichage pour le distinguer des boutons.
        L'ajout d'un bouton "Quitter" pour permettre à l'utilisateur de fermer l'application.
        L'ajout d'un padding uniforme pour les boutons et les cellules de la grille.
        La modification du comportement du bouton "C" pour effacer le champ de saisie et réinitialiser l'affichage à "0".
        La modification du comportement du bouton "0" pour éviter qu'il y ait plusieurs zéros à gauche du nombre affiché.

--- pyculator.py
+++ pyculator.py
@@ -1,46 +1,70 @@
 import tkinter as tk
 
 
-# todo make better UI
-class PyCalculator:
+class Calculator:
     def __init__(self, master):
         self.master = master
         master.title("Calculatrice")
+        master.resizable(False, False)
 
-        # Entry and display system
-        self.display = tk.Entry(master, width=30, font=('Arial', 16))
-        self.display.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+        # Couleurs
+        button_color = "#eeeeee"
+        button_active_color = "#cccccc"
+        display_color = "#ffffff"
+        display_active_color = "#f0f0f0"
+        border_color = "#dddddd"
 
-        # Buttons
-        btns = [
+        # Affichage
+        self.display = tk.Entry(master, width=30, font=('Arial', 16), bd=0, justify='right', bg=display_color,
+                                highlightthickness=1, highlightbackground=border_color,
+                                highlightcolor=display_active_color)
+        self.display.grid(row=0, column=0, columnspan=5, padx=5, pady=5, sticky='nsew')
+        self.display.insert(0, "0")
+
+        # Boutons
+        buttons = [
             '7', '8', '9', '/', 'C',
             '4', '5', '6', '*', '±',
             '1', '2', '3', '-', 'x²',
             '0', '.', '=', '+', '√'
         ]
 
-        # Buttons creation
-
+        # Création des boutons
         row = 1
         col = 0
-        for btn in btns:
-            command = lambda x=btn: self.click(x)
-            tk.Button(master, text=btn, width=5, height=2, command=command).grid(row=row, column=col, padx=5, pady=5)
+        for button in buttons:
+            command = lambda x=button: self.click(x)
+            tk.Button(master, text=button, width=5, height=2, bg=button_color, activebackground=button_active_color,
+                      relief='flat', command=command).grid(row=row, column=col, padx=5, pady=5, sticky='nsew')
             col += 1
-            print(col)
             if col > 4:
                 col = 0
                 row += 1
 
+        # Ajouter un bouton pour quitter l'application
+        tk.Button(master, text="Quitter", width=8, height=2, bg=button_color, activebackground=button_active_color,
+                  relief='flat', command=master.quit).grid(row=5, column=4, padx=5, pady=5, sticky='nsew')
+
+        # Appliquer un padding uniforme aux boutons et aux cellules de la grille
+        for i in range(5):
+            master.columnconfigure(i, weight=1)
+        for i in range(6):
+            master.rowconfigure(i, weight=1)
+
     def click(self, key):
-        if key == "=":
-            # Result
-            result = eval(self.display.get())
-            self.display.delete(0, tk.END)
-            self.display.insert(0, str(result))
+        if key == '=':
+            # Calculer le résultat
+            try:
+                result = eval(self.display.get())
+                self.display.delete(0, tk.END)
+                self.display.insert(0, str(result))
+            except:
+                self.display.delete(0, tk.END)
+                self.display.insert(0, "Erreur")
         elif key == 'C':
-            # Canceled
+            # Effacer le champ de saisie
             self.display.delete(0, tk.END)
+            self.display.insert(0, "0")
         elif key == '±':
             # Changer le signe du nombre affiché
             value = self.display.get()
@@ -70,4 +94,12 @@ class PyCalculator:
                 self.display.insert(0, "Erreur")
         else:
             # Ajouter la touche appuyée au champ de saisie
+            if self.display.get() == "0":
+                self.display.delete(0)
             self.display.insert(tk.END, key)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    calculator = Calculator(root)
+    root.mainloop()
