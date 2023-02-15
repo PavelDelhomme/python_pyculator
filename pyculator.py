6d96ff767d44a37deb32ec085e38d48aa8220e17
PavelDelhomme <pavel@delhomme.ovh>
2023-02-15 14:35:52 +0100

    V2 more functionnality

--- pyculator.py
+++ pyculator.py
@@ -13,10 +13,10 @@ class PyCalculator:
 
         # Buttons
         btns = [
-            "7", "8", "9", "/",
-            "4", "5", "6", "*",
-            "1", "2", "3", "-",
-            ".", "0", "=", "+"
+            '7', '8', '9', '/', 'C',
+            '4', '5', '6', '*', '±',
+            '1', '2', '3', '-', 'x²',
+            '0', '.', '=', '+', '√'
         ]
 
         # Buttons creation
@@ -28,7 +28,7 @@ class PyCalculator:
             tk.Button(master, text=btn, width=5, height=2, command=command).grid(row=row, column=col, padx=5, pady=5)
             col += 1
             print(col)
-            if col > 3:
+            if col > 4:
                 col = 0
                 row += 1
 
@@ -41,5 +41,33 @@ class PyCalculator:
         elif key == 'C':
             # Canceled
             self.display.delete(0, tk.END)
+        elif key == '±':
+            # Changer le signe du nombre affiché
+            value = self.display.get()
+            if value and value[0] == '-':
+                self.display.delete(0)
+            else:
+                self.display.insert(0, '-')
+        elif key == 'x²':
+            # Calculer le carré du nombre affiché
+            try:
+                value = float(self.display.get())
+                result = value ** 2
+                self.display.delete(0, tk.END)
+                self.display.insert(0, str(result))
+            except:
+                self.display.delete(0, tk.END)
+                self.display.insert(0, "Erreur")
+        elif key == '√':
+            # Calculer la racine carrée du nombre affiché
+            try:
+                value = float(self.display.get())
+                result = value ** 0.5
+                self.display.delete(0, tk.END)
+                self.display.insert(0, str(result))
+            except:
+                self.display.delete(0, tk.END)
+                self.display.insert(0, "Erreur")
         else:
+            # Ajouter la touche appuyée au champ de saisie
             self.display.insert(tk.END, key)
